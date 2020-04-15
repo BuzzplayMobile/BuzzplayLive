e05cf566e96425074f7c99276a29ed9d62edd455
Ben Hornedo <ben@uptown4.com>
2020-04-16 00:11:23 +0300

    Added upcoming shows list HTML to show page

--- src/pages/show/show-page.js
+++ src/pages/show/show-page.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import Hero from "../../components/hero/hero";
+import ShowItem from "../../components/show-item/show-item";
 
 class ShowPage extends React.Component {
   render() {
     // const { path, url } = this.props.match;
     // const showId = url.substring(url.lastIndexOf("/") + 1);
+    const upcomingShows = [
+      { id: 0 },
+      { id: 1 },
+      { id: 2 },
+      { id: 3 },
+      { id: 4 },
+      { id: 5 },
+      { id: 6 },
+      { id: 7 },
+      { id: 8 },
+      { id: 9 },
+      { id: 10 },
+      { id: 11 },
+      { id: 12 },
+      { id: 13 },
+      { id: 14 },
+      { id: 15 },
+    ];
     return (
       <div className="show-page__container">
         <Hero image="https://unsplash.it/1500?random">
@@ -25,6 +44,14 @@ class ShowPage extends React.Component {
             Mauris euismod ante ac purus consequat ornare in eu tortor.
           </p>
           <button>Join LiveStream</button>
+        </div>
+        <div className="show-page__upcoming">
+          <h2 className="show-page__upcoming-list--title">Upcoming shows</h2>
+          <div className="show-page__upcoming-list">
+            {upcomingShows.map((show) => (
+              <ShowItem key={show.id} show={show} />
+            ))}
+          </div>
         </div>
       </div>
     );
